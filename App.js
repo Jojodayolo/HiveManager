40a30660db92ce5021fd2e50e7b9292457874883
Jojo <info@jonathan-wittkop.de>
2022-12-13 15:12:33 +0100

    - Added a Header using Navigation Module - Switched to dark mode(maybe only IOS) - created a src folder and moved the asset folder to ther, also added a screen folder which contains an example page(LocationOverview.js)

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Appearance } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import {LocationOverview} from './src/screens/LocationOverview';
+import { DefaultTheme, DarkTheme } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    </View>
+  );
+}
 
 export default function App() {
+const colorScheme = Appearance.getColorScheme();
+
   return (
-    <View style={styles.container}>
-      <Text>hello world</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={DarkTheme}>
+      <Stack.Navigator>
+        <Stack.Screen
+        name="Hive Manager"
+        component={LocationOverview}
+        backgroundColor="#fff"
+        options={{ title: 'HiveManager'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  TitleBar:{
+
   },
 });
---
+++ src/screens/LocationOverview.js
@@ -0,0 +1,12 @@
+import { DarkTheme } from '@react-navigation/native';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export const LocationOverview = () => {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} >
+        <Text style={{color: 'white'}}>LocationOverview</Text>
+      </View>
+    );
+  } 
+
