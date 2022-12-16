9a3dc7f700eb1f64ebe81daaa64df0ae2e21a819
Marcel Witkowski <marcel.witkowski@stud.hs-ruhrwest.de>
2022-12-16 14:57:48 +0100

    -1:2 Layout for LocationViewer

--- src/screens/LocationViewer.js
+++ src/screens/LocationViewer.js
@@ -1,12 +1,23 @@
 import { DarkTheme } from '@react-navigation/native';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, FlatList, Text, View } from 'react-native';
 
 export const LocationViewer = () => {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} >
-        <Text style={{color: 'white'}}>LocationViewer</Text>
+      <View style={[styles.container, {flexDirection: "row"}]} >
+        <View style={{ flex: 1, backgroundColor: "black" }}>
+            <Text style={{color: 'list'}}>hives</Text>
+        </View> 
+        <View style={{ flex: 2, backgroundColor: "white" }}>
+            <Text style={{color: 'black'}}>documentations</Text>
+        </View>
       </View>
+      
     );
   } 
 
+  const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+      },
+    });
